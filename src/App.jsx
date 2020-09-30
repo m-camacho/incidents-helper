a391b8d108d6b48959f89d4bb597a14118a0397c
m-camacho <mario.camacho2@ymail.com>
2020-09-30 18:38:56 -0400

    Including Sass and Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import './App.scss';
+
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import AppContext from './AppContext';
-import * as serviceWorker from './serviceWorker';
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import AppContext from "./AppContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <AppContext>
-    <App />
+      <App />
     </AppContext>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
