fa31f6542012b4bdd56dcc54a6f19164aaf7dbe1
mario camacho <mario.camacho2@ymail.com>
2020-09-29 21:43:20 -0400

    Adding basic app context

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppContext from './AppContext';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+    <AppContext>
     <App />
+    </AppContext>
   </React.StrictMode>,
   document.getElementById('root')
 );
